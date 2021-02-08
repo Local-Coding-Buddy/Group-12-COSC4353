fc4eba26a9310f7c88e102168a568aa73dfc45bb
Jaffar <67613243+ajaffar0311@users.noreply.github.com>
2021-02-08 15:28:40 -0600

    Created files for the project

---
+++ FuelRatePrediction/FuelRatePredSys/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+    path("", views.index, name='index'),
+    path("home/", views.home, name='home'),
+    path("login/", views.login, name='login'),
+    path("register/", views.register, name='register'),
+    path("profile/", views.profile, name='profile'),
+
+]
--- FuelRatePrediction/FuelRatePredSys/views.py
+++ FuelRatePrediction/FuelRatePredSys/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+
+def index(request):
+	return render(request, 'FuelRatePredSys/home.html')
+
+def home(request):
+	 return render(request, 'FuelRatePredSys/home.html')
+
+def login(request):
+	return render(request, 'FuelRatePredSys/login.html')
+
+def register(request):
+	return render(request, 'FuelRatePredSys/register.html')
+
+def profile(request):
+	return render(request, 'FuelRatePredSys/profile.html')
