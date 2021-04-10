8cd143d39fdb73fb359c62515c3bac452d68f514
ajaffar0311 <67613243+ajaffar0311@users.noreply.github.com>
2021-04-10 14:39:24 -0500

    fixed admin bug

--- FuelRatePrediction/FuelRatePredSys/admin.py
+++ FuelRatePrediction/FuelRatePredSys/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import UserProfile, Pricing_Module
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Pricing_Module)
+
+
+
+
+
+# Re-register UserAdmin
---
+++ FuelRatePrediction/FuelRatePredSys/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+
+#app_name = "FQ"
+urlpatterns = [
+    path("", views.index, name='index'),
+    path("home/", views.home, name='home'),
+    path("register/", views.register, name='register'),
+    path("profile/",views.profile, name ='profile'),
+    path("profile_management/",views.profile_management, name ='profile_management'),
+    path('fqh/', views.quote_history, name='FuelRatePredSys-fqh'),
+    path('fqf/', views.quote_form, name='FuelRatePredSys-fqf'),
+
+]
